6c3ae8eaf72e43123615551dfb629765f963cd6e
Alex I <42416891+t0020979@users.noreply.github.com>
2022-02-22 20:03:52 +0000

    Задание 3

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-dates = []
-tasks = []
-dates.append(input('Введите дату: '))
-tasks.append(input('Введите задачу: '))
-dates.append(input('Введите дату: '))
-tasks.append(input('Введите задачу: '))
-dates.append(input('Введите дату: '))
-tasks.append(input('Введите задачу: '))
+planner = {}
 
-print(dates[0], tasks[0])
-print(dates[1], tasks[1])
-print(dates[2], tasks[2])
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+planner[date] = task
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+planner[date] = task
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+planner[date] = task
+
+# print(planner)
 
