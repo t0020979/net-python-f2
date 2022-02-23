6e78a9af3648ca6d4b21750be81ed0514f5137f4
Alex I <42416891+t0020979@users.noreply.github.com>
2022-02-23 13:44:13 +0000

    Задание 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+words = ['python', 'c++', 'c', 'scala', 'java'] 
+letter = 'c'
+
+def count_letter(words, letter):
+  total = 0
+  for word in words:
+    if letter in word:
+      total += 1
+  return total
+
+total = count_letter(words, letter)
+print(f'Total words:{total} with letter {letter}')
