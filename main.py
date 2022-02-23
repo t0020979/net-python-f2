58207e09af8e379695027b2a1acdcdf09ded0d0c
Alex I <42416891+t0020979@users.noreply.github.com>
2022-02-23 11:58:38 +0000

    Задание 1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello, World!")
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
+
+tasks = []
+
+run = True
+
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    print(tasks)
+  elif command == "add":
+    task = input("Введите название задачи: ")
+    tasks.append(task)
+    print("Задача добавлена")
+  elif command == "exit":
+    break
+  else: 
+    print("Неизвестная команда")
+
+print("Спасибо за использование! До свидания!")
