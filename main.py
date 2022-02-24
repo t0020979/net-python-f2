88a7a8fefe09a1e11190850e6a0437dd5de8266c
Alex I <42416891+t0020979@users.noreply.github.com>
2022-02-24 07:45:05 +0000

    Задание 1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import telebot
+
+token = '5181869878:AAEJvx_0qNkXOdVk0Og-BpNIUwCv-hAEWzY'
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+  if 'Alex' in message.text:
+    message.text = 'Ба! Знакомые все лица!'
+  bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
